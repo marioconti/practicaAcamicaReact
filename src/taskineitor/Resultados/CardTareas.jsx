9953231cddf3bb346bfba125b3428d22815b388a
Mario Conti <marioConti91@gmail.com>
2021-08-03 10:58:26 -0300

    se agrega al repositorio estados en titulo, fecha e i de basurero

--- src/taskineitor/Resultados/CardTareas.jsx
+++ src/taskineitor/Resultados/CardTareas.jsx
@@ -1,36 +1,84 @@
 import React from "react";
-import { tareas } from "./arrayTaskineitor";
+import { tareas } from "../arrayTaskineitor";
 import "./Card.css";
 
 export const Tareas = () => {
+  // se hace toda la lógica del mapeo antes del return. Declaraciones de variables que funcionarán dentro del scoup
   let variable = tareas.map((e) => {
+    let fecha1 = new Date().getTime() / 1000;
+    let fecha2 = new Date(e.fechaCreacion).getTime() / 1000;
+    let fechaTarea = fecha1 - fecha2;
+    let fechaTareaDias = fechaTarea / 60 / 60 / 24;
+    let fechaDiaEntero = fechaTareaDias.toFixed(0);
+
     return (
+      // instanciación de la función que mapeará el componente, trayendo cada vez que pasé por los objetos del array los valores de cada propiedad
       <>
         <CardTareas
           titulo={e.titulo}
           fechaCreacion={e.fechaCreacion}
           categoria={e.categoria}
+          // en este caso el valor no está sacado del array en principio, sino de las declaraciones de variables que se hicieron antes del return, que igualmente forman parte del array (es decir, que igual se repetiran en cada indice) porque yo la estoy solicitando en este caso en el template string
+          fechas={fechaDiaEntero}
         />
       </>
     );
   });
+
   return variable;
 };
 
 export const CardTareas = (props) => {
+  const infoTarea = () => {
+    console.log(`Esta es la tarea: ${props.titulo}`);
+  };
+
+  const infoFecha = () => {
+    console.log(`Su fecha de creación es: ${props.fechaCreacion}`);
+  };
+
+  const borrarTarea = () => {
+    const fechaHoyLarga = new Date();
+    // hay alguna forma que no salga todo el chorizo ese en ingles? Esta se me ocurre pero no se si es práctica
+    const fechaAño = fechaHoyLarga.getFullYear();
+    const fechaMes = fechaHoyLarga.getMonth()+1;
+    const fechaDia = fechaHoyLarga.getDate();
+
+    console.log("¿Deseas borrar esta tarea?");
+    alert(
+      "Deseas borrar -" +
+        props.titulo +
+        " - hoy " +
+        fechaDia +
+        "/" +
+        fechaMes +
+        "/" +
+        fechaAño
+    );
+  };
+
   return (
     <div className="card-container">
-      <p className="tarea-titulo">{props.titulo}</p>
-      <div className="tarea-fecha">
-        <p className="fecha">{props.fechaCreacion}</p>
+      <p onClick={infoTarea} className="tarea-titulo">
+        {props.titulo}
+      </p>
+      <div onClick={infoFecha} className="tarea-fecha">
+        <p className="fecha">
+          {props.fechaCreacion} - {`Hace ${props.fechas} días`}
+        </p>
       </div>
       <div className="tarea-categoria">
         <p>Categoria:</p>
         <p className="categoria">{props.categoria}</p>
       </div>
       <div className="contenedor-svg">
-        <i class="imagen-svg far fa-square"></i>
-        <i class="imagen-svg fas fa-trash-alt"></i>
+        <div>
+          <i className="imagen-svg far fa-square"></i>
+        </div>
+        {/*FIXME: ¿Por que tengo que poner el evento en un div para que funcione y no puedo hacer la invocación directamente en el i? */}
+        <div onClick={borrarTarea}>
+          <i className="imagen-svg fas fa-trash-alt"></i>
+        </div>
       </div>
     </div>
   );
--- src/taskineitor/arrayTaskineitor.js
+++ src/taskineitor/arrayTaskineitor.js
@@ -1,37 +1,37 @@
 export let tareas = [
   {
     titulo: "Practicar javascript",
-    fechaCreacion: "12/20/2020",
+    fechaCreacion: "12-20-2020",
     categoria: "estudios"
   },
   {
     titulo: "Ir al gimnasio",
-    fechaCreacion: "1/1/2021",
+    fechaCreacion: "1-1-2021",
     categoria: "salud"
   },
   {
     titulo: "Llamar al doctor",
-    fechaCreacion: "1/2/2021",
+    fechaCreacion: "1-2-2021",
     categoria: "salud"
   },
   {
     titulo: "Buscar el paquete",
-    fechaCreacion: "12/20/2020",
+    fechaCreacion: "12-20-2020",
     categoria: "organizacion"
   },
   {
     titulo: "Practicar frances",
-    fechaCreacion: "11/15/2020",
+    fechaCreacion: "11-15-2020",
     categoria: "estudios"
   },
   {
     titulo: "Lavar los platos",
-    fechaCreacion: "1/05/2021",
+    fechaCreacion: "1-05-2021",
     categoria: "organizacion"
   },
   {
     titulo: "Recoger mi cuarto",
-    fechaCreacion: "1/08/2021",
+    fechaCreacion: "1-08-2021",
     categoria: "organizacion"
   },
 ];
